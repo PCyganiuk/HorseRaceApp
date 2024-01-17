8b364f0044717a1bf50c7170ccc4dceafefb1af2
rivaldoxxx <maciej.ilow.pwr@gmail.com>
2024-01-17 19:27:17 +0100

    admin controller

---
+++ src/main/java/com/example/horseraceapp/AdminMenuController.java
@@ -0,0 +1,23 @@
+package com.example.horseraceapp;
+
+import javafx.event.ActionEvent;
+
+public class AdminMenuController {
+    public void handleAddRemoveHorse(ActionEvent actionEvent) {
+
+    }
+
+    public void handleSchedule(ActionEvent actionEvent) {
+
+    }
+
+    public void handleHorseStats(ActionEvent actionEvent) {
+    }
+
+    public void handleConfirmWin(ActionEvent actionEvent) {
+    }
+
+    public void handleControlOdds(ActionEvent actionEvent) {
+
+    }
+}
